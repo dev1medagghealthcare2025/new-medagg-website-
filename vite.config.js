58702ac4180497afe12736f0dfc3f6a64cb1fded
dev1medagghealthcare2025 <dev1.medagghealthcare@gmail.com>
2025-11-10 15:25:45 +0530

    white screen problem fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Ensure assets are referenced from the root in production
+  base: '/',
 });
